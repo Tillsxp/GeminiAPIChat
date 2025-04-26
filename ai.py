0f3b2dfc8e5df28b355a77566c346c55fede2182
Matilda Söderberg <matilda.89@live.se>
2025-04-26 17:48:57 +0200

    Change to multi-turn converstation

--- ai.py
+++ ai.py
@@ -7,9 +7,15 @@ load_dotenv(dotenv_path="secret.env")
 api_key=os.getenv("API_KEY")
 
 client = genai.Client(api_key=api_key)  
+chat = client.chats.create(model="gemini-2.0-flash")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+response = chat.send_message_stream(
+    "Explain how AI works in a few words"
 )
+for chunk in response:
+    print(chunk.text, end="")
 
-print(response.text)
+response = chat.send_message_stream("What did I ask in my previous question?")
+for message in chat.get_history():
+    print(f'role - {message.role}', end =": ")
+    print(message.parts[0].text)
